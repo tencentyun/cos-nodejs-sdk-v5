5dfcce1c527014a8ae503486b706e8739e06841b
carsonxu <459452372@qq.com>
2017-07-11 13:12:36 +0800

    fix restart bug

--- sdk/advance.js
+++ sdk/advance.js
@@ -543,8 +543,8 @@ function uploadSliceItem(params, callback) {
         start: start,
         end: end - 1
     });
-
-    var ContentSha1 = UploadData.PartList[PartNumber - 1].ETag;
+    var PartItem = UploadData.PartList[PartNumber - 1];
+    var ContentSha1 = PartItem.ETag;
     Async.retry(sliceRetryTimes, function (tryCallback) {
         if (!self._isRunningTask(TaskId)) return;
         self.multipartUpload({
@@ -563,6 +563,7 @@ function uploadSliceItem(params, callback) {
             if (err) {
                 return tryCallback(err);
             } else {
+                PartItem.Uploaded = true;
                 return tryCallback(null, data);
             }
         });

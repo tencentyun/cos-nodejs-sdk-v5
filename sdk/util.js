740ceb96ad84a7677b9f101c52832a3cf1dd7f59
carsonxu <459452372@qq.com>
2017-09-06 22:05:42 +0800

    update

--- sdk/util.js
+++ sdk/util.js
@@ -25,6 +25,7 @@ var getAuth = function (opt) {
     var pathname = opt.pathname || '/';
     var queryParams = opt.params || '';
     var headers = opt.headers || '';
+    pathname.indexOf('/') === -1 && (pathname = '/' + pathname);
 
     if (!SecretId) return console.error('lack of param SecretId');
     if (!SecretKey) return console.error('lack of param SecretKey');

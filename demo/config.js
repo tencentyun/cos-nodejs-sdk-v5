9dcdbd0e8487868e9e9e976e0b0f8b888ba15c48
Wonder <8199240+Wonder233@users.noreply.github.com>
2019-08-06 19:53:02 +0800

    增加配置文件 config.js

---
+++ demo/config.js
@@ -0,0 +1,8 @@
+var config = {
+    SecretId: 'AKIDxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+    SecretKey: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+    Bucket: 'test-1250000000',
+    Region: 'ap-guangzhou'
+};
+
+module.exports = config;

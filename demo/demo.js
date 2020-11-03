959b842afaecebfc8e72c1d2032530d03bfddf9d
carsonxu <g459452372>
2020-11-03 15:38:37 +0800

    update demo

--- demo/demo.js
+++ demo/demo.js
@@ -923,7 +923,6 @@ function selectObjectContentStream() {
     selectStream.on('end', () => console.log(fs.readFileSync(outFile).toString()))
 }
 
-selectObjectContent()
 function selectObjectContent() {
     // // 如果返回结果很大，可以用 selectObjectContentStream 处理
     // // 查询 CSV

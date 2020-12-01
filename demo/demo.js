caeef743b61b9da8548b5a274c169da3f0b91132
carsonxu <g459452372>
2020-12-01 15:56:26 +0800

    update demo

--- demo/demo.js
+++ demo/demo.js
@@ -15,7 +15,7 @@ var cos = new COS({
     ChunkParallelLimit: 8,   // 控制单个文件下分片上传并发数，在同园区上传可以设置较大的并发数
     ChunkSize: 1024 * 1024 * 8,  // 控制分片大小，单位 B，在同园区上传可以设置较大的分片大小
     Proxy: '',
-    Protocol: 'http:',
+    Protocol: 'https:',
 });
 
 var TaskId;

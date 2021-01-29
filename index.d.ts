f2711390b19c11a1e672f69a48b1bd807c3833e0
carsonxu <g459452372>
2021-01-29 19:07:35 +0800

    add Expires

--- index.d.ts
+++ index.d.ts
@@ -195,10 +195,12 @@ declare namespace COS {
     Query?: Query,
     /** 要参与签名计算的 Header 参数，可选 */
     Headers?: Headers,
-    /** 校正时间的偏移值，单位 ms(毫秒)，计算签名时会用设备当前时间戳加上该偏移值，在设备时间有误时可用于校正签名用的时间参数。 */
-    SystemClockOffset?: number,
+    /** 签名几秒后失效，默认为900秒，如果传入了 KeyTime，以 KeyTime 为准，可选 */
+    Expires?: number,
     /** 签名有效时间戳区间，如果传入了该参数，会赋值给在签名里的 q-key-time 和 q-sign-time 字段，格式如：1611915436;1611916336 */
     KeyTime?: string,
+    /** 校正时间的偏移值，单位 ms(毫秒)，计算签名时会用设备当前时间戳加上该偏移值，在设备时间有误时可用于校正签名用的时间参数。 */
+    SystemClockOffset?: number,
   }
   /** 计算签名或获取临时密钥可能需要的参数列表 */
   interface GetAuthorizationOptions {

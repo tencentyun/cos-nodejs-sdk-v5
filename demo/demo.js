0a5650dcb1c547321bd3f43ca704c3414d6b65f4
carsonxu <459452372@qq.com>
2021-07-20 19:53:36 +0800

    Update demo.js

--- demo/demo.js
+++ demo/demo.js
@@ -1214,10 +1214,8 @@ function uploadFile() {
               var speed = Math.floor(info.speed / 1024 / 1024 * 100) / 100;
               console.log('进度：' + percent + '%; 速度：' + speed + 'Mb/s;');
           },
-          onFileFinish: function (err, data, options) {
-              console.log(options.Key + ' 上传' + (err ? '失败' : '完成'));
-          },
-      }, function (err, data) {
+      }, function (err, data) {{
+          console.log('上传' + (err ? '失败' : '完成'));
           console.log(err || data);
           fs.unlinkSync(filepath);
       });

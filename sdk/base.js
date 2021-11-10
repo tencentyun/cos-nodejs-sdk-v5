ce5378c8aec80490b293b9af219f0d7857553b90
carsonxu <459452372@qq.com>
2021-11-10 11:46:24 +0800

    优化 getSignHost 方法，判断 ci/pic 域名

--- sdk/base.js
+++ sdk/base.js
@@ -3253,17 +3253,16 @@ function getUrl(params) {
 
 var getSignHost = function (opt) {
     if (!opt.Bucket || !opt.Bucket) return '';
-    var ps = this.options.ForcePathStyle;
     var url = opt.Url || getUrl({
-        ForcePathStyle: ps,
+        ForcePathStyle: this.options.ForcePathStyle,
         protocol: this.options.Protocol,
         domain: this.options.Domain,
         bucket: opt.Bucket,
         region: opt.Region,
     });
-    var standardHost = (ps ? '' : opt.Bucket + '.') + 'cos.' + opt.Region + '.myqcloud.com';
     var urlHost = url.replace(/^https?:\/\/([^/]+)(\/.*)?$/, '$1');
-    if (standardHost === urlHost) return standardHost;
+    var standardHostReg = new RegExp('^([a-z\\d-]+-\\d+\\.)?(cos|cosv6|ci|pic)\\.([a-z\\d-]+)\\.myqcloud\\.com$');
+    if (standardHostReg.test(urlHost)) return urlHost;
     return '';
 }
 

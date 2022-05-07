e5276e01caeed026e0aa333db927601433ff6f43
chrisftian <chrisftian@tencent.com>
2022-05-07 17:50:29 +0800

    feat：补充d.ts

--- index.d.ts
+++ index.d.ts
@@ -1881,6 +1881,8 @@ Bulk：批量模式，恢复时间为24 - 48小时。 */
     QueryString?: string,
     /** 签名几秒后失效，默认为900秒 */
     Expires?: number,
+    /** 是否使用全球加速 */
+    UseAccelerate?: boolean,
   }
   /** getObjectUrl 接口返回值 */
   interface GetObjectUrlResult {

7f612b170765c80f61b705b51b81742bdaa79189
chrisftian <enter1994@qq.com>
2022-06-23 11:55:36 +0800

    Feat/2.11.12 (#148)
    * feat 2.11.12
    
    * upd d.ts
    
    Co-authored-by: chrisftian <chrisftian@tencent.com>

--- sdk/base.js
+++ sdk/base.js
@@ -3411,7 +3411,9 @@ function getAuthorizationAsync(params, callback) {
     })();
 
     var calcAuthByTmpKey = function () {
-        var KeyTime = StsData.StartTime && StsData.ExpiredTime ? StsData.StartTime + ';' + StsData.ExpiredTime : '';
+        var KeyTime = '';
+        if (StsData.StartTime && params.Expires) KeyTime = StsData.StartTime + ';' + (StsData.StartTime + params.Expires * 1);
+        else if (StsData.StartTime && StsData.ExpiredTime) KeyTime = StsData.StartTime + ';' + StsData.ExpiredTime;
         var Authorization = util.getAuth({
             SecretId: StsData.TmpSecretId,
             SecretKey: StsData.TmpSecretKey,
